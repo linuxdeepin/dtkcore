ec6e2a8376c7aca7162b4fbb782b998c9a6ab630
Iceyer <me@iceyer.net>
2018-01-15 20:29:01 +0800

    fix: set default build version for debian changelog
    Change-Id: Ia1099685e8119ce050f3ee3a7bb9c39cec387496

--- cmake/DtkCore/DtkCoreConfig.cmake
+++ cmake/DtkCore/DtkCoreConfig.cmake
@@ -1,4 +1,4 @@
 
-set(INCLUDE_INSTALLED_DIR "/usr/include/libdtk-2.0/DCore")
+set(INCLUDE_INSTALLED_DIR "/usr/include/libdtk-2.0.6/DCore")
 set(DtkCore_LIBRARIES dtkcore)
 include_directories("${INCLUDE_INSTALLED_DIR}")
