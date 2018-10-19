be9f1da1317b25b1a73dfe3eab9d5e01e80509d5
haruyukilxz <lxz@ilxz.me>
2018-10-19 11:30:37 +0800

    doc: add chinese documentation for DDiskSizeFormatter
    Change-Id: Ia2f5467d2d571eadaeb1be53e4bb4d04ecc216c7

--- src/util/ddisksizeformatter.cpp
+++ src/util/ddisksizeformatter.cpp
@@ -21,12 +21,50 @@
 
 DCORE_BEGIN_NAMESPACE
 
+/*!
+ * \~chinese \class DDiskSizeFormatter
+ *
+ * \~chinese \brief DDiskSizeFormatter 是用来获取磁盘容量单位的类, 通过枚举值
+ * 获取不同类型磁盘容量的单位
+ *
+ * \~chinese \enum DDiskSizeFormatter::DiskUnits 磁盘容量单位的枚举
+ * \~chinese \var DDiskSizeFormatter::DiskUnits DDiskSizeFormatter::B
+ * \~chinese \brief 字节
+ * \~chinese \var DDiskSizeFormatter::DiskUnits DDiskSizeFormatter::K
+ * \~chinese \brief 千字节
+ * \~chinese \var DDiskSizeFormatter::DiskUnits DDiskSizeFormatter::M
+ * \~chinese \brief 兆字节
+ * \~chinese \var DDiskSizeFormatter::DiskUnits DDiskSizeFormatter::G
+ * \~chinese \brief 吉字节
+ * \~chinese \var DDiskSizeFormatter::DiskUnits DDiskSizeFormatter::T
+ * \~chinese \brief 太字节
+ *
+ * \~chinese \fn DDiskSizeFormatter::unitMax
+ * \~chinese \brief 返回最大磁盘容量单位的枚举
+ *
+ * \~chinese \fn DDiskSizeFormatter::unitMin
+ * \~chinese \brief 返回最小磁盘容量单位的枚举
+ *
+ * \~chinese \fn DDiskSizeFormatter::unitConvertRate
+ * \~chinese \brief 返回当前的单位转换比率
+ */
+
+/*!
+ * \~chinese \brief DDiskSizeFormatter的构造函数
+ *
+ */
 DDiskSizeFormatter::DDiskSizeFormatter()
     : DAbstractUnitFormatter()
 {
 
 }
 
+/*!
+ * \~chinese \brief 根据枚举返回对应单位的字符串
+ *
+ * @param unitId DDiskSizeFormatter::DiskUnits 的枚举值
+ * @return QString 对应单位的字符串
+ */
 QString DDiskSizeFormatter::unitStr(int unitId) const
 {
     switch (unitId)
@@ -41,6 +79,12 @@ QString DDiskSizeFormatter::unitStr(int unitId) const
     return QString();
 }
 
+/*!
+ * \~chinese \brief 设置当前的单位转换比率
+ *
+ * @param rate 转换比率
+ * @return DDiskSizeFormatter 返回 DDiskSizeFormatter 对象
+ */
 DDiskSizeFormatter DDiskSizeFormatter::rate(int rate)
 {
     m_rate = rate;
