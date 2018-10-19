2deb442545725adaf3b6b98b15220c02e16bf358
haruyukilxz <lxz@ilxz.me>
2018-10-19 11:11:25 +0800

    doc: add chinese documentation for dtimeunitformatter
    Change-Id: I4c655f9c3c1466d77796d4191718a220617a1192

--- src/util/dtimeunitformatter.cpp
+++ src/util/dtimeunitformatter.cpp
@@ -21,12 +21,45 @@
 
 DCORE_BEGIN_NAMESPACE
 
+/*!
+ * \~chinese \class DTimeUnitFormatter
+ *
+ * \~chinese \brief DTimeUnitFormatter是用来获取时间单位的类, 通过枚举值
+ * 获取不同类型时间单位的进制
+ *
+ * \~chinese \enum DTimeUnitFormatter::TimeUnits 时间单位的枚举
+ * \~chinese \var DTimeUnitFormatter::TimeUnits DTimeUnitFormatter::Seconds
+ * \~chinese \brief 返回分钟单位的进制
+ * \~chinese \var DTimeUnitFormatter::TimeUnits DTimeUnitFormatter::Minute
+ * \~chinese \brief 返回秒单位的进制
+ * \~chinese \var DTimeUnitFormatter::TimeUnits DTimeUnitFormatter::Hour
+ * \~chinese \brief 返回小时单位的进制
+ * \~chinese \var DTimeUnitFormatter::TimeUnits DTimeUnitFormatter::Day
+ * \~chinese \brief 返回天单位的进制
+ *
+ * \~chinese \fn DTimeUnitFormatter::unitMax
+ * \~chinese \brief 返回最大时间单位的枚举
+ *
+ * \~chinese \fn DTimeUnitFormatter::unitMin
+ * \~chinese \brief 返回最小时间单位的枚举
+ */
+
+/*!
+ * \~chinese \brief DTimeUnitFormatter的构造函数
+ *
+ */
 DTimeUnitFormatter::DTimeUnitFormatter()
     : DAbstractUnitFormatter()
 {
 
 }
 
+/*!
+ * \~chinese \brief 根据枚举返回对应的单位进制
+ *
+ * @param unitId DTimeUnitFormatter::TimeUnits 的枚举值
+ * @return uint 对应的单位进制
+ */
 uint DTimeUnitFormatter::unitConvertRate(int unitId) const
 {
     switch (unitId)
@@ -40,6 +73,12 @@ uint DTimeUnitFormatter::unitConvertRate(int unitId) const
     return 0;
 }
 
+/*!
+ * \~chinese \brief 根据枚举返回对应单位的缩写
+ *
+ * @param unitId DTimeUnitFormatter::TimeUnits 的枚举值
+ * @return QString 对应单位的缩写
+ */
 QString DTimeUnitFormatter::unitStr(int unitId) const
 {
     switch (unitId)
