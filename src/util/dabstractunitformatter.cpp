c51431e699c4997a892568c0fc894bba603d4259
haruyukilxz <lxz@ilxz.me>
2018-10-22 09:30:24 +0800

    doc: add chinese documentation for DAbstractUnitFormatter
    Change-Id: Ia9248147c31b664750f6531f5d0abc8d193efd3b

--- src/util/dabstractunitformatter.cpp
+++ src/util/dabstractunitformatter.cpp
@@ -19,16 +19,57 @@
 
 DCORE_BEGIN_NAMESPACE
 
+/*!
+ * \~chinese \class DAbstractUnitFormatter
+ * \~chinese \brief DAbstractUnitFormatter 类是对拥有相同类型数据管理的接口类
+ * 接口定义了最大值、最小值、转换单位和单位对应的字符串。
+ *
+ * \~chinese \fn DAbstractUnitFormatter::unitMax
+ * \~chinese \brief 返回列表中最大的单位
+ *
+ * \~chinese \fn DAbstractUnitFormatter::unitMin
+ * \~chinese \brief 返回列表中最小的单位
+ *
+ * \~chinese \fn DAbstractUnitFormatter::unitConvertRate
+ * \~chinese \brief 返回当前设置的转换单位
+ *
+ * \~chinese \fn DAbstractUnitFormatter::unitValueMax
+ * \~chinese \brief 返回列表中根据当前设置的转换单位的最大值
+ *
+ * \~chinese \fn DAbstractUnitFormatter::unitValueMin
+ * \~chinese \brief 返回列表中根据当前设置的转换单位的最小值
+ *
+ * \~chinese \fn DAbstractUnitFormatter::unitStr
+ * \~chinese \brief 传入id，返回列表中对应的字符串
+ */
+
+/*!
+ * \~chinese \brief DAbstractUnitFormatter 的构造函数
+ *
+ */
 DAbstractUnitFormatter::DAbstractUnitFormatter()
 {
 
 }
 
+/*!
+ * \~chinese \brief DAbstractUnitFormatter 的析构函数
+ *
+ */
 DAbstractUnitFormatter::~DAbstractUnitFormatter()
 {
 
 }
 
+/*!
+ * \~chinese \brief 将传入的值从当前转换单位转换到目标单位上，返回转换过的值
+ * 如果当前转换单位小于目标单位，值会被缩小，反之会放大，当前转换单位也会被缩小和放大，直至当前转换单位等于目标单位。
+ *
+ * @param value 原始数值
+ * @param currentUnit 当前的转换比率
+ * @param targetUnit 目标的转换比率
+ * @return qreal 返回转换过的值
+ */
 qreal DAbstractUnitFormatter::formatAs(qreal value, int currentUnit, const int targetUnit) const
 {
     while (currentUnit < targetUnit)
@@ -39,6 +80,15 @@ qreal DAbstractUnitFormatter::formatAs(qreal value, int currentUnit, const int t
     return value;
 }
 
+/*!
+ * \~chinese \brief 将值转换到最合适的单位上
+ *
+ * 如果值大于 unitMin() 或者小于 unitMax() ，会尽量保证值被转换到接近最小值的合适单位上。
+ *
+ * @param value 原始数值
+ * @param unit 当前的转换单位
+ * @return QPair<qreal, int> 转换过的数值和转化单位
+ */
 QPair<qreal, int> DAbstractUnitFormatter::format(const qreal value, const int unit) const
 {
     // can convert to smaller unit
@@ -52,6 +102,13 @@ QPair<qreal, int> DAbstractUnitFormatter::format(const qreal value, const int un
     return QPair<qreal, int>(value, unit);
 }
 
+/*!
+ * \~chinese \brief 是 format() ，但是包含了完整的转换数据
+ *
+ * @param value
+ * @param unit
+ * @return QList<QPair<qreal, int> >
+ */
 QList<QPair<qreal, int> > DAbstractUnitFormatter::formatAsUnitList(const qreal value, int unit) const
 {
     if (qFuzzyIsNull(value))
