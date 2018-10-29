0d12b7244dfa9927a84575df09c5752ca78e155e
Iceyer <me@iceyer.net>
2018-10-29 22:02:22 +0800

    docs: DPathBuf
    Change-Id: I625dbfeaa2f0008b3fff7890fdf0f2d2d92e04eb

--- src/filesystem/dpathbuf.cpp
+++ src/filesystem/dpathbuf.cpp
@@ -1,5 +1,14 @@
 #include "dpathbuf.h"
 
+/*!
+ * \class DPathBuf
+ * \brief DPathBuf cat path friendly and supoort multiplatform.
+ */
+
+/*!
+ * \brief Create DPathBuf from a string.
+ * \param path
+ */
 Dtk::Core::DPathBuf::DPathBuf(const QString &path)
 {
     m_path = QDir(path).absolutePath();
