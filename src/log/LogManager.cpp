e2a299d3414e048dfc5c479ab670b53b9a5678f0
ck <kegechen@gmail.com>
2021-04-07 12:04:55 +0800

    fix: logfile path bugs
    1.root应用如果HOME环境变量没有设置时，默认获取到的缓存路径变成/.cache，添加判断rootpath==homepath则不初始化日志路径
    2.主动设置日志路径时如果为目录路径(existed)，则警告后什么都不做
    
    Log: 修复日志路径的问题
    Change-Id: I2302c605e5e51c168bf7938a447f30459f83e1d3
    (cherry picked from commit 0f18bcc89c2627a559dec8e549634cf1fd873ce3)

--- src/log/LogManager.cpp
+++ src/log/LogManager.cpp
@@ -66,12 +66,23 @@ void DLogManager::registerFileAppender() {
 
 //! Return the path file log storage
 /**
+ * \~chinese \brief DLogManager::getlogFilePath 获取日志文件路径
+ * \~chinese \brief 默认日志路径是 ~/.cache/organizationName/applicationName.log
+ * \~chinese \brief 如果获取 HOME 环境变量失败将不写日志
  * \sa registerFileAppender
  */
-QString DLogManager::getlogFilePath(){
+QString DLogManager::getlogFilePath()
+{
     // 不再构造时去设置默认logpath(且mkdir), 而在getlogPath时再去判断是否设置默认值
     // 修复设置了日志路径还是会在默认的位置创建目录的问题
     if (DLogManager::instance()->m_logPath.isEmpty()) {
+        if (QDir::homePath() == QDir::rootPath()) {
+            qWarning() << "unable to locate the cache directory."
+                       << "logfile path is empty, the log will not be written.\r\n"
+                       << (qgetenv("HOME").isEmpty() ? "the HOME environment variable not set" : "");
+            return QString();
+        }
+
         QString cachePath = QStandardPaths::standardLocations(QStandardPaths::CacheLocation).at(0);
         if (!QDir(cachePath).exists()) {
             QDir(cachePath).mkpath(cachePath);
@@ -82,15 +93,16 @@ QString DLogManager::getlogFilePath(){
     return QDir::toNativeSeparators(DLogManager::instance()->m_logPath);
 }
 
-
 /*!
  * \~chinese \brief DLogManager::setlogFilePath 设置日志文件路径
  * \~chinese \param logFilePath 日志文件路径
+ * \~chinese \brief 如果设置的文件路进不是文件路径将什么都不做，输出一条警告
  */
 void DLogManager::setlogFilePath(const QString &logFilePath)
 {
-    if (QDir(logFilePath).exists())
-        DLogManager::instance()->m_logPath = DLogManager::instance()->joinPath(logFilePath, QString("%1.log").arg(qApp->applicationName()));
+    QFileInfo info(logFilePath);
+    if (info.exists() && !info.isFile())
+        qWarning() << "invalid file path:" << logFilePath << " is not a file";
     else
         DLogManager::instance()->m_logPath = logFilePath;
 }
