5fb1c1812e789d9296c438d0044a6439efa1654e
zccrs <zccrs@live.com>
2021-08-26 17:59:57 +0800

    feat: 添加  DDciFileEngine
    DDciFileEngine 实现了 QAbstractFileEngine，可以直接使用 QFile、
    QFileInfo 等 Qt 的文件接口操作 DCI 文件。将打包的 DCI 文件看作一个
    目录，类似于 "qrc"，绑定 "dci:" 开头的路径，以识别对 DCI 文件进行的
    IO 操作。
    
    Log:
    Change-Id: Iaff407294b211fa869cd4ad74322098160fdb9b5

--- src/dci/dci.pri
+++ src/dci/dci.pri
@@ -4,10 +4,12 @@ PUBLIC_HEADERS += \
     $$PWD/ddcifile.h
 
 HEADERS += \
-    $$PUBLIC_HEADERS
+    $$PUBLIC_HEADERS \
+    $$PWD/private/ddcifileengine_p.h
 
 SOURCES += \
-    $$PWD/ddcifile.cpp
+    $$PWD/ddcifile.cpp \
+    $$PWD/private/ddcifileengine.cpp
 
 includes.files += \
     $$PUBLIC_HEADERS \
--- tests/ut_ddci.cpp
+++ tests/ut_ddci.cpp
@@ -20,7 +20,13 @@
  */
 
 #include <DDciFile>
+
 #include <QLoggingCategory>
+#include <QDir>
+#include <QDirIterator>
+#include <QFile>
+#include <QFileInfo>
+#include <QDateTime>
 
 #include <gtest/gtest.h>
 
@@ -29,7 +35,8 @@ DCORE_USE_NAMESPACE
 class ut_DCI : public testing::Test {
 public:
     void SetUp() override {
-        QLoggingCategory::setFilterRules("dtk.dci.file.debug=true");
+        QLoggingCategory::setFilterRules("dtk.dci.file.debug=true\n"
+                                         "dtk.dci.fileengine.debug=true");
     }
 };
 
@@ -140,5 +147,167 @@ TEST_F(ut_DCI, DDciFile) {
         ASSERT_EQ(dciFile.list("/"), QStringList{});
         ASSERT_EQ(dciFile.childrenCount("/"), 0);
         ASSERT_EQ(dciFile.toData(), QByteArrayLiteral("DCI\0\1\0\0\0"));
+    }
+}
+
+class TestDCIFileHelper {
+public:
+    TestDCIFileHelper(const QString &fileName)
+        : fileName(fileName)
+    {
+        if (QFile::exists(fileName))
+            QFile::remove(fileName);
+    }
+    ~TestDCIFileHelper() {
+        QFile::remove(fileName);
+    }
+
+    inline QString dciFormatFilePath(const QString &subfile = QString()) const {
+        return "dci:" + fileName + subfile;
+    }
+
+    inline QString sourceFileName() const {
+        return fileName;
+    }
+
+private:
+    QString fileName;
+};
+
+TEST_F(ut_DCI, DFileEngine) {
+    {
+        TestDCIFileHelper helper(QDir::temp().absoluteFilePath("test.dci"));
+        // 空 dci 文件创建
+        QFile file(helper.dciFormatFilePath());
+        ASSERT_TRUE(file.exists());
+        QFileInfo info(file);
+        ASSERT_TRUE(info.isDir());
+        // 文件夹不可写入
+        ASSERT_FALSE(file.open(QIODevice::WriteOnly));
+        // 空文件遍历
+        QDir dir(info.absoluteFilePath());
+        ASSERT_EQ(dir.entryList(), QStringList{});
+    }
+
+    {
+        TestDCIFileHelper helper(QDir::temp().absoluteFilePath("test.dci"));
+        {
+            // 内部文件创建
+            QFile file(helper.dciFormatFilePath("/test.txt"));
+            ASSERT_FALSE(file.open(QIODevice::ReadOnly));
+            ASSERT_TRUE(file.open(QIODevice::ReadOnly | QIODevice::WriteOnly));
+            ASSERT_TRUE(file.exists());
+            ASSERT_TRUE(QFileInfo(file).isFile());
+            ASSERT_TRUE(file.write("Hello") == 5);
+            ASSERT_TRUE(file.seek(0));
+            ASSERT_EQ(file.readAll(), "Hello");
+            ASSERT_TRUE(file.flush());
+            file.close();
+
+            // 文件信息
+            QFileInfo info1(file);
+            QFileInfo info2(helper.sourceFileName());
+            ASSERT_EQ(info1.permissions(), info2.permissions());
+            ASSERT_EQ(info1.fileTime(QFile::FileAccessTime),
+                      info2.fileTime(QFile::FileAccessTime));
+            ASSERT_EQ(info1.fileTime(QFile::FileBirthTime),
+                      info2.fileTime(QFile::FileBirthTime));
+            ASSERT_EQ(info1.fileTime(QFile::FileMetadataChangeTime),
+                      info2.fileTime(QFile::FileMetadataChangeTime));
+            ASSERT_EQ(info1.fileTime(QFile::FileModificationTime),
+                      info2.fileTime(QFile::FileModificationTime));
+            ASSERT_EQ(info1.ownerId(), info2.ownerId());
+            ASSERT_EQ(info1.owner(), info2.owner());
+            ASSERT_EQ(info1.groupId(), info2.groupId());
+            ASSERT_EQ(info1.group(), info2.group());
+
+            // 目录遍历
+            QDir dir(helper.dciFormatFilePath());
+            ASSERT_EQ(dir.entryList(), QStringList{"test.txt"});
+
+            // 文件大小
+            ASSERT_EQ(file.size(), 5);
+            ASSERT_TRUE(file.resize(10));
+            ASSERT_EQ(file.size(), 10);
+        }
+
+        {
+            // 文件读取
+            QFile file(helper.dciFormatFilePath("/test.txt"));
+            ASSERT_TRUE(file.exists());
+            ASSERT_TRUE(file.open(QIODevice::ReadOnly));
+            ASSERT_EQ(file.readAll(), QByteArrayLiteral("Hello\0\0\0\0\0"));
+            file.close();
+            // [/test.txt]
+        }
+
+        {
+            // 文件内容改写
+            QFile file(helper.dciFormatFilePath("/test.txt"));
+            ASSERT_TRUE(file.open(QIODevice::ReadWrite));
+            ASSERT_TRUE(file.seek(1));
+            ASSERT_TRUE(file.putChar('E'));
+            char ch;
+            ASSERT_TRUE(file.getChar(&ch));
+            ASSERT_EQ(ch, 'l');
+            ASSERT_EQ(file.readAll(), QByteArrayLiteral("lo\0\0\0\0\0"));
+            ASSERT_TRUE(file.seek(0));
+            ASSERT_EQ(file.readAll(), QByteArrayLiteral("HEllo\0\0\0\0\0"));
+        }
+
+        // 目录创建
+        ASSERT_TRUE(QDir(helper.dciFormatFilePath()).mkdir("1"));
+        ASSERT_FALSE(QDir(helper.dciFormatFilePath()).mkdir("2/3"));
+        ASSERT_TRUE(QDir(helper.dciFormatFilePath()).mkpath("2/3"));
+        ASSERT_TRUE(QFileInfo(helper.dciFormatFilePath("/1")).isDir());
+        // [/test.txt, /1, /2, /2/3]
+
+        // 目录 rename
+        ASSERT_FALSE(QFile::rename(helper.dciFormatFilePath("/1"), "/1"));
+        ASSERT_TRUE(QFile::rename(helper.dciFormatFilePath("/1"),
+                                  helper.dciFormatFilePath("/1.new")));
+        ASSERT_TRUE(QFile::rename(helper.dciFormatFilePath("/2"),
+                                  helper.dciFormatFilePath("/2.new")));
+        ASSERT_TRUE(QFile::rename(helper.dciFormatFilePath("/2.new/3"),
+                                  helper.dciFormatFilePath("/3")));
+        // [/test.txt, /1.new, /2.new, /3]
+
+        // 文件 rename
+        ASSERT_TRUE(QFile::rename(helper.dciFormatFilePath("/test.txt"),
+                                  helper.dciFormatFilePath("/test.txt.new")));
+        ASSERT_TRUE(QFile::rename(helper.dciFormatFilePath("/test.txt.new"),
+                                  helper.dciFormatFilePath("/1.new/test.txt")));
+        // [/1.new, /1.new/test.txt.new, /2.new, /3]
+
+        // 复制
+        ASSERT_TRUE(QFile::copy(helper.dciFormatFilePath("/1.new/test.txt"),
+                                helper.dciFormatFilePath("/test.txt")));
+        // 目录禁止复制
+        ASSERT_FALSE(QFile::copy(helper.dciFormatFilePath("/3"),
+                                 helper.dciFormatFilePath("/3.new")));
+        // [/1.new, /1.new/test.txt, /2.new, /3, /test.txt]
+
+        // 目录遍历
+        QStringList list {
+            helper.dciFormatFilePath("/1.new"),
+            helper.dciFormatFilePath("/1.new/test.txt"),
+            helper.dciFormatFilePath("/2.new"),
+            helper.dciFormatFilePath("/3"),
+            helper.dciFormatFilePath("/test.txt")
+        };
+        QDirIterator di(helper.dciFormatFilePath(), QDirIterator::Subdirectories);
+        while (di.hasNext()) {
+            const QString &file = di.next();
+            ASSERT_TRUE(list.removeOne(file));
+        }
+        ASSERT_TRUE(list.isEmpty());
+
+        // 删除
+        ASSERT_TRUE(QFile::remove(helper.dciFormatFilePath("/test.txt")));
+        ASSERT_TRUE(QFile::remove(helper.dciFormatFilePath("/2.new")));
+        ASSERT_TRUE(QFile::remove(helper.dciFormatFilePath("/1.new")));
+        // [/3]
+        ASSERT_EQ(QDir(helper.dciFormatFilePath()).entryList(),
+                  QStringList {"3"});
     }
 }
