6867a279be8c2cefbc57ef0433f7c27dea04c3a2
ck <chenke@uniontech.com>
2022-03-04 16:14:51 +0800

    chore: 修改单元测试生成目录
    修改单元测试相关文件的目录结构
    
    Log:
    Influence: 单元测试目录结构
    Change-Id: Ife2dabc8763fd6689c7b5c5fadbc40152217e116

--- tests/test-recoverage-qmake.sh
+++ tests/test-recoverage-qmake.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-BUILD_DIR=build
-REPORT_DIR=report
+BUILD_DIR=`pwd`/../build-ut
+HTML_DIR=${BUILD_DIR}/html
+XML_DIR=${BUILD_DIR}/report
 #EXTRACT_ARGS="src"
 cd ../
 rm -rf $BUILD_DIR
@@ -16,16 +17,13 @@ mkdir $BUILD_DIR
 cd $BUILD_DIR
 qmake ../ CONFIG+=debug
 export ASAN_OPTIONS=halt_on_error=0
-TESTARGS="--gtest_output=xml:dde_test_report_dtkcore.xml"  make check -j$(nproc)
+TESTARGS="--gtest_output=xml:${XML_DIR}/report_dtkcore.xml"  make check -j$(nproc)
 
 lcov -d ./ -c -o coverage_all.info
 #lcov --extract coverage_all.info $EXTRACT_ARGS --output-file coverage.info
-lcov --remove coverage_all.info "*/tests/*" "*/usr/include*" "*build/src*" --output-file coverage.info
+lcov --remove coverage_all.info "*/tests/*" "*/usr/include*" "*build/src*" "*build-ut/src*" --output-file coverage.info
 cd ..
-genhtml -o $REPORT_DIR $BUILD_DIR/coverage.info
+genhtml -o $HTML_DIR $BUILD_DIR/coverage.info && mv ${BUILD_DIR}/html/index.html ${BUILD_DIR}/html/cov_dtkcore.html
 
-#rm -rf $BUILD_DIR
-#rm -rf ../$BUILD_DIR
-
-test -e ./build/asan.log* && mv ./build/asan.log* ./build/asan_dtkcore.log || touch ./build/asan.log
+test -e ${BUILD_DIR}/asan.log* && mv ${BUILD_DIR}/asan.log* ${BUILD_DIR}/asan_dtkcore.log || touch ${BUILD_DIR}/asan.log
 
