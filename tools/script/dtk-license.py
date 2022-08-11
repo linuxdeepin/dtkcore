3c662582b04ad9616c2efa12eca932a5cbfaf11d
guoyao <guoyao@uniontech.com>
2022-08-11 13:02:07 +0800

    chore: adapt REUSE license header
    开源合规
    
    Log: 开源合规

--- tools/script/dtk-license.py
+++ tools/script/dtk-license.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
 import fnmatch
 import os
 import argparse
