1750f9811b8208c03485d53d0a473538742ae7f4
xzl <xiangzelong@uniontech.com>
2022-08-19 10:15:21 +0800

    chore: update REUSE license header
    更新开源合规内容
    
    Log: 更新开源合规内容

--- include/log/win32/OutputDebugAppender.h
+++ include/log/win32/OutputDebugAppender.h
@@ -1,16 +1,7 @@
-/*
-  Copyright (c) 2010 Karl-Heinz Reichel (khreichel at googlemail dot com)
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Lesser General Public License version 2.1
-  as published by the Free Software Foundation and appearing in the file
-  LICENSE.LGPL included in the packaging of this file.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Lesser General Public License for more details.
-*/
+// SPDX-FileCopyrightText: 2010 Karl-Heinz Reichel (khreichel at googlemail dot com)
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
 
 #ifndef OUTPUTDEBUGAPPENDER_H
 #define OUTPUTDEBUGAPPENDER_H
