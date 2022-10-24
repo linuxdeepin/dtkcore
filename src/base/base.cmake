a32e231fd40ef58b01f48d209ddd8fb1c708f7d6
heyuming <heyuming@uniontech.com>
2022-10-24 10:24:48 +0800

    chore: add CMake rules for installing dexpected.h
    以及derror.h
    
    Log: 添加dexpected.h的CMake安装规则

--- src/base/base.cmake
+++ src/base/base.cmake
@@ -2,5 +2,7 @@ set(base_SRCS
   ${CMAKE_CURRENT_LIST_DIR}/../../include/base/dobject.h
   ${CMAKE_CURRENT_LIST_DIR}/../../include/base/private/dobject_p.h
   ${CMAKE_CURRENT_LIST_DIR}/../../include/base/dsingleton.h
+  ${CMAKE_CURRENT_LIST_DIR}/../../include/base/dexpected.h
+  ${CMAKE_CURRENT_LIST_DIR}/../../include/base/derror.h
   ${CMAKE_CURRENT_LIST_DIR}/dobject.cpp
 )
