bf4235dc8a095fe206d3b64317b4fa202b0a713e
chenhongtao <chenhongtao@uniontech.com>
2022-11-21 09:25:50 +0800

    chore: fix license
    Log: fix license in win32/OutputDebugAppender.h

--- include/log/win32/OutputDebugAppender.h
+++ include/log/win32/OutputDebugAppender.h
@@ -1,4 +1,3 @@
-// SPDX-FileCopyrightText: 2010 Karl-Heinz Reichel (khreichel at googlemail dot com)
 // SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
