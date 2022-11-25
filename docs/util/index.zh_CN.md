94e51e3a71dc33c41d6bd585bc0566e80c224325
Astrea <astreaonearth@gmail.com>
2022-11-25 18:20:01 +0800

    docs: update docs for util
    Update docs for dabstractunitformatter.h.
    
    Log: update docs
    Influence: none

---
+++ docs/util/index.zh_CN.md
@@ -0,0 +1,11 @@
+# DTK util
+
+@mainpage
+@defgroup dutil
+
+## 模块介绍
+
+该模块提供一些实用工具用于开发，包括:
+
++ 同种数据类的单位转换接口
++ DBus接口工具类
--- src/util/dabstractunitformatter.cpp
+++ src/util/dabstractunitformatter.cpp
@@ -7,139 +7,151 @@
 DCORE_BEGIN_NAMESPACE
 
 /*!
-  \class Dtk::Core::DAbstractUnitFormatter
-  \inmodule dtkcore
-  \brief DAbstractUnitFormatter 类是对拥有相同类型数据管理的接口类.
-  
-  接口定义了最大值、最小值、转换单位和单位对应的字符串。
+  @~english
+  @class Dtk::Core::DAbstractUnitFormatter
+  @ingroup dutil
+  @brief DAbstractUnitFormatter is a interface which manages the data with the
+  same type
+
+  DAbstractUnitFormatter provides interfaces including: <br>
+    1. The maximum value of one unit.
+    2. The minumum value of one unit.
+    3. The convert rate to next unit.
+    4. The string to display one unit.
  */
 
 /*!
-  \fn int DAbstractUnitFormatter::unitMax() const = 0
-  \brief 返回列表中最大的单位.
+  @~english
+  @fn int DAbstractUnitFormatter::unitMax() const = 0
+  @brief Get the maximum unit in the list
  */
 /*!
-  \fn int DAbstractUnitFormatter::unitMin() const = 0
-  \brief 返回列表中最小的单位.
+  @~english
+  @fn int DAbstractUnitFormatter::unitMin() const = 0
+  @brief Get the minumum unit in the list
  */
 /*!
-  \fn uint DAbstractUnitFormatter::unitConvertRate(int unitId) const = 0
-  \brief 返回当前设置的转换单位.
-  \a unitId 单元ID.
+  @~english
+  @fn uint DAbstractUnitFormatter::unitConvertRate(int unitId) const = 0
+  @brief Get the convert rate to next unit of unitId
+  @param[in] unitId the id of one unit
  */
 /*!
-  \fn qreal DAbstractUnitFormatter::unitValueMax(int unitId) const
-  \brief 返回列表中根据当前设置的转换单位的最大值.
-  \a unitId 单元ID.
+  @~english
+  @fn qreal DAbstractUnitFormatter::unitValueMax(int unitId) const
+  @brief Get the maximum value of unitId
+  @param[in] unitId the id of one unit
  */
 /*!
-  \fn qreal DAbstractUnitFormatter::unitValueMin(int unitId) const
-  \brief 返回列表中根据当前设置的转换单位的最小值.
-  \a unitId 单元ID.
+  @~english
+  @fn qreal DAbstractUnitFormatter::unitValueMin(int unitId) const
+  @brief Get the minumum value of unitId
+  @param[in] unitId the id of one unit
  */
 /*!
-  \fn QString DAbstractUnitFormatter::unitStr(int unitId) const = 0
-  \brief 传入id，返回列表中对应的字符串.
-  \a unitId 单元ID.
+  @~english
+  @fn QString DAbstractUnitFormatter::unitStr(int unitId) const = 0
+  @brief Get the display string of unitId
+  @param[in] unitId the id of one unit
  */
 
 /*!
-  \brief DAbstractUnitFormatter 的构造函数.
-  
- */
-DAbstractUnitFormatter::DAbstractUnitFormatter()
-{
+  @~english
+  @brief Constructor of DAbstractUnitFormatter
 
+ */
+DAbstractUnitFormatter::DAbstractUnitFormatter() {}
+
+/*!
+  @~english
+  @brief Destructor of DAbstractUnitFormatter
+
+ */
+DAbstractUnitFormatter::~DAbstractUnitFormatter() {}
+
+/*!
+  @~english
+  @brief Convert value from unit currentUnit to targetUnit. If currentUnit is
+  smaller than targetUnit, the value will zoom out until the unit is equal to
+  targetUnit. Instead it will zoom in.
+
+  @param[in] value primitive value
+  @param[in] currentUnit current unit of value
+  @param[in] targetUnit target unit to convert to
+  @return qreal the value with unit targetUnit
+ */
+qreal DAbstractUnitFormatter::formatAs(qreal value, int currentUnit,
+                                       const int targetUnit) const {
+  while (currentUnit < targetUnit)
+    value /= unitConvertRate(currentUnit++);
+  while (currentUnit > targetUnit)
+    value *= unitConvertRate(--currentUnit);
+
+  return value;
 }
 
 /*!
-  \brief DAbstractUnitFormatter 的析构函数
-  
- */
-DAbstractUnitFormatter::~DAbstractUnitFormatter()
-{
+  @~english
+  @brief Convert the value to a proper unit.
 
+  If unit is larger than unitMin() or smaller than unitMax, value will be
+  converted to the unit where value is close to the unitValueMin()
+
+  @param[in] value primitive value
+  @param[in] unit current unit
+  @return QPair<qreal, int> a pair of the converted value and unit
+ */
+QPair<qreal, int> DAbstractUnitFormatter::format(const qreal value,
+                                                 const int unit) const {
+  // can convert to smaller unit
+  if (unit > unitMin() && value < unitValueMin(unit))
+    return format(value * unitConvertRate(unit - 1), unit - 1);
+
+  // can convert to bigger unit
+  if (unit < unitMax() && value > unitValueMax(unit))
+    return format(value / unitConvertRate(unit), unit + 1);
+
+  return QPair<qreal, int>(value, unit);
 }
 
 /*!
-  \brief 将传入的值从当前转换单位转换到目标单位上，返回转换过的值
-  如果当前转换单位小于目标单位，值会被缩小，反之会放大，当前转换单位也会被缩小和放大，直至当前转换单位等于目标单位。
-  
-  \a value 原始数值
-  \a currentUnit 当前的转换比率
-  \a targetUnit 目标的转换比率
-  \return qreal 返回转换过的值
+  @~english
+  @brief A version of format() with all the convert data
+
+  @param[in] value primitive value
+  @param[in] unit current unit
+  @return QList<QPair<qreal, int>> pairs of all converted value and unit
  */
-qreal DAbstractUnitFormatter::formatAs(qreal value, int currentUnit, const int targetUnit) const
-{
-    while (currentUnit < targetUnit)
-        value /= unitConvertRate(currentUnit++);
-    while (currentUnit > targetUnit)
-        value *= unitConvertRate(--currentUnit);
+QList<QPair<qreal, int>>
+DAbstractUnitFormatter::formatAsUnitList(const qreal value, int unit) const {
+  if (qFuzzyIsNull(value))
+    return QList<QPair<qreal, int>>();
 
-    return value;
-}
+  if (value < unitValueMin(unit) || unit == unitMin()) {
+    if (unit != unitMin())
+      return formatAsUnitList(value * unitConvertRate(unit - 1), unit - 1);
+    else
+      return std::move(QList<QPair<qreal, int>>()
+                       << QPair<qreal, int>(value, unit));
+  }
 
-/*!
-  \brief 将值转换到最合适的单位上
-  
-  如果值大于 unitMin() 或者小于 unitMax() ，会尽量保证值被转换到接近最小值的合适单位上。
-  
-  \a value 原始数值
-  \a unit 当前的转换单位
-  \return QPair<qreal, int> 转换过的数值和转化单位
- */
-QPair<qreal, int> DAbstractUnitFormatter::format(const qreal value, const int unit) const
-{
-    // can convert to smaller unit
-    if (unit > unitMin() && value < unitValueMin(unit))
-        return format(value * unitConvertRate(unit - 1), unit - 1);
+  ulong _value = ulong(value);
+  QList<QPair<qreal, int>> ret = formatAsUnitList(value - _value, unit);
 
-    // can convert to bigger unit
-    if (unit < unitMax() && value > unitValueMax(unit))
-        return format(value / unitConvertRate(unit), unit + 1);
+  while (_value && unit != unitMax()) {
+    const ulong rate = unitConvertRate(unit);
+    const ulong r = _value % rate;
+    if (r)
+      ret.push_front(QPair<qreal, int>(r, unit));
 
-    return QPair<qreal, int>(value, unit);
-}
+    unit += 1;
+    _value /= rate;
+  }
 
-/*!
-  \brief 是 format() ，但是包含了完整的转换数据
-  
-  \a value
-  \a unit
-  \return QList<QPair<qreal, int> >
- */
-QList<QPair<qreal, int> > DAbstractUnitFormatter::formatAsUnitList(const qreal value, int unit) const
-{
-    if (qFuzzyIsNull(value))
-        return QList<QPair<qreal, int>>();
+  if (_value)
+    ret.push_front(QPair<qreal, int>(_value, unit));
 
-    if (value < unitValueMin(unit) || unit == unitMin())
-    {
-        if (unit != unitMin())
-            return formatAsUnitList(value * unitConvertRate(unit - 1), unit - 1);
-        else
-            return std::move(QList<QPair<qreal, int>>() << QPair<qreal, int>(value, unit));
-    }
-
-    ulong _value = ulong(value);
-    QList<QPair<qreal, int>> ret = formatAsUnitList(value - _value, unit);
-
-    while (_value && unit != unitMax())
-    {
-        const ulong rate = unitConvertRate(unit);
-        const ulong r = _value % rate;
-        if (r)
-            ret.push_front(QPair<qreal, int>(r, unit));
-
-        unit += 1;
-        _value /= rate;
-    }
-
-    if (_value)
-        ret.push_front(QPair<qreal, int>(_value, unit));
-
-    return ret;
+  return ret;
 }
 
 DCORE_END_NAMESPACE
