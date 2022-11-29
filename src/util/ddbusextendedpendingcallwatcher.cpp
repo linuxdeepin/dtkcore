7e35e1455810028e2843d02d729740e0279fb21c
Astrea <astreaonearth@gmail.com>
2022-11-29 17:06:36 +0800

    chore!: move namespace
    Move DDBusExtendedAbstractInterface and DDBusExtendedPendingCallWatcher
    to Dtk::Core.
    
    Log: move namespace
    Influence: DDBusAbstractInterface, DDBusExtendedPendingCallWatcher

--- src/util/ddbusextendedpendingcallwatcher.cpp
+++ src/util/ddbusextendedpendingcallwatcher.cpp
@@ -4,14 +4,12 @@
 
 #include "ddbusextendedpendingcallwatcher_p.h"
 
+DCORE_BEGIN_NAMESPACE
+DDBusExtendedPendingCallWatcher::DDBusExtendedPendingCallWatcher(
+    const QDBusPendingCall &call, const QString &asyncProperty,
+    const QVariant &previousValue, QObject *parent)
+    : QDBusPendingCallWatcher(call, parent), m_asyncProperty(asyncProperty),
+      m_previousValue(previousValue) {}
 
-DDBusExtendedPendingCallWatcher::DDBusExtendedPendingCallWatcher(const QDBusPendingCall &call, const QString &asyncProperty, const QVariant &previousValue, QObject *parent)
-    : QDBusPendingCallWatcher(call, parent)
-    , m_asyncProperty(asyncProperty)
-    , m_previousValue(previousValue)
-{
-}
-
-DDBusExtendedPendingCallWatcher::~DDBusExtendedPendingCallWatcher()
-{
-}
+DDBusExtendedPendingCallWatcher::~DDBusExtendedPendingCallWatcher() {}
+DCORE_END_NAMESPACE
