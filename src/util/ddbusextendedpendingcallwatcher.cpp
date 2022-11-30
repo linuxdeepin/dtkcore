724696bdf6a93c984873ed3df35552959f953019
Astrea <astreaonearth@gmail.com>
2022-11-30 15:54:20 +0800

    chore: format code
    Add .clang-format. Format code that has been modified by commit
    e6423c5.
    
    Log: format code
    Influence: e6423c5

--- src/util/ddbusextendedpendingcallwatcher.cpp
+++ src/util/ddbusextendedpendingcallwatcher.cpp
@@ -5,11 +5,15 @@
 #include "ddbusextendedpendingcallwatcher_p.h"
 
 DCORE_BEGIN_NAMESPACE
-DDBusExtendedPendingCallWatcher::DDBusExtendedPendingCallWatcher(
-    const QDBusPendingCall &call, const QString &asyncProperty,
-    const QVariant &previousValue, QObject *parent)
-    : QDBusPendingCallWatcher(call, parent), m_asyncProperty(asyncProperty),
-      m_previousValue(previousValue) {}
+DDBusExtendedPendingCallWatcher::DDBusExtendedPendingCallWatcher(const QDBusPendingCall &call,
+                                                                 const QString &asyncProperty,
+                                                                 const QVariant &previousValue,
+                                                                 QObject *parent)
+    : QDBusPendingCallWatcher(call, parent)
+    , m_asyncProperty(asyncProperty)
+    , m_previousValue(previousValue)
+{
+}
 
 DDBusExtendedPendingCallWatcher::~DDBusExtendedPendingCallWatcher() {}
 DCORE_END_NAMESPACE
