5686a7e8c34c85629fd7e1ffcc9053606f17d38c
xzl <xiangzelong@uniontech.com>
2023-01-30 10:38:35 +0800

    feat: add journal appenders
    support log to journal
    
    Log:

--- src/log/log.cmake
+++ src/log/log.cmake
@@ -1,24 +1,30 @@
 file(GLOB LOG_HEADER
   ${CMAKE_CURRENT_LIST_DIR}/../../include/log/*.h
 )
-set(LOG_SOURCE 
-  ${CMAKE_CURRENT_LIST_DIR}/RollingFileAppender.cpp 
-  ${CMAKE_CURRENT_LIST_DIR}/Logger.cpp 
-  ${CMAKE_CURRENT_LIST_DIR}/FileAppender.cpp 
-  ${CMAKE_CURRENT_LIST_DIR}/ConsoleAppender.cpp 
-  ${CMAKE_CURRENT_LIST_DIR}/AbstractStringAppender.cpp 
-  ${CMAKE_CURRENT_LIST_DIR}/AbstractAppender.cpp 
+set(LOG_SOURCE
+  ${CMAKE_CURRENT_LIST_DIR}/RollingFileAppender.cpp
+  ${CMAKE_CURRENT_LIST_DIR}/Logger.cpp
+  ${CMAKE_CURRENT_LIST_DIR}/FileAppender.cpp
+  ${CMAKE_CURRENT_LIST_DIR}/ConsoleAppender.cpp
+  ${CMAKE_CURRENT_LIST_DIR}/AbstractStringAppender.cpp
+  ${CMAKE_CURRENT_LIST_DIR}/AbstractAppender.cpp
   ${CMAKE_CURRENT_LIST_DIR}/LogManager.cpp
 )
+if(UNIX AND NOT APPLE)
+  list(APPEND LOG_SOURCE
+  ${CMAKE_CURRENT_LIST_DIR}/JournalAppender.cpp
+  )
+endif()
+
 if(WIN32)
-  set(log_SRCS 
+  set(log_SRCS
     ${LOG_HEADER}
     ${LOG_SOURCE}
     ${CMAKE_CURRENT_LIST_DIR}/OutputDebugAppender.cpp
     ${CMAKE_CURRENT_LIST_DIR}/../../include/log/win32/OutputDebugAppender.h
   )
 else()
-  set(log_SRCS 
+  set(log_SRCS
     ${LOG_HEADER}
     ${LOG_SOURCE}
   )
