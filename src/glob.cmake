33de26ff1cecee8fd00a84810934a2dbede1b339
Ye ShanShan <yeshanshan@uniontech.com>
2023-02-03 14:51:56 +0800

    fix: Losing dconfig info when compiling and packaging
      Add adjust for linux when compile.
      Add `D_DSG_APP_DATA_FALLBACK` macro when packaging.
      see the commit: 7b13e5c4b867fcbdcc50f5e409b0c8726496b7e5
    
    Log: none
    Influence: none
    Change-Id: Ie047dbd2d8158ed5242dfb5bcc7fa4e02ee1d925

--- src/glob.cmake
+++ src/glob.cmake
@@ -1,29 +1,42 @@
+set(OUTER_SOURCE
+  ${CMAKE_CURRENT_LIST_DIR}/dconfig.cpp
+  ${CMAKE_CURRENT_LIST_DIR}/dsgapplication.cpp
+  ${CMAKE_CURRENT_LIST_DIR}/dsysinfo.cpp
+  ${CMAKE_CURRENT_LIST_DIR}/dsecurestring.cpp
+  ${CMAKE_CURRENT_LIST_DIR}/ddesktopentry.cpp
+  ${CMAKE_CURRENT_LIST_DIR}/dtkcore_global.cpp
+)
+set(OUTER_HEADER
+  ${CMAKE_CURRENT_LIST_DIR}/../include/global/dtkcore_global.h
+  ${CMAKE_CURRENT_LIST_DIR}/../include/global/dconfig.h
+  ${CMAKE_CURRENT_LIST_DIR}/../include/global/dsgapplication.h
+  ${CMAKE_CURRENT_LIST_DIR}/../include/global/dsysinfo.h
+  ${CMAKE_CURRENT_LIST_DIR}/../include/global/dsecurestring.h
+  ${CMAKE_CURRENT_LIST_DIR}/../include/global/ddesktopentry.h
+)
+
 if(LINUX)
-  file(GLOB OUTER_SOURCE
-    ${CMAKE_CURRENT_LIST_DIR}/*.cpp
+  if(DEFINED D_DSG_APP_DATA_FALLBACK)
+      add_definitions(-DD_DSG_APP_DATA_FALLBACK="${D_DSG_APP_DATA_FALLBACK}")
+  endif()
+  list(APPEND OUTER_SOURCE
+    ${CMAKE_CURRENT_LIST_DIR}/dconfigfile.cpp
   )
-  file(GLOB OUTER_HEADER
-    ${CMAKE_CURRENT_LIST_DIR}/../include/global/*.h 
+  list(APPEND OUTER_HEADER
+    ${CMAKE_CURRENT_LIST_DIR}/../include/global/dconfigfile.h
   )
+#   generic dbus interfaces
+  if(NOT DEFINED DTK_DISABLE_DBUS_CONFIG)
+    include(${CMAKE_CURRENT_LIST_DIR}/dbus/dbus.cmake)
+    list(APPEND glob_SRC ${dbus_SRCS})
+  else()
+    add_definitions(-DD_DISABLE_DBUS_CONFIG)
+  endif()
 else()
-  set(OUTER_SOURCE 
-    ${CMAKE_CURRENT_LIST_DIR}/dconfig.cpp 
-    ${CMAKE_CURRENT_LIST_DIR}/dsgapplication.cpp 
-    ${CMAKE_CURRENT_LIST_DIR}/dsysinfo.cpp 
-    ${CMAKE_CURRENT_LIST_DIR}/dsecurestring.cpp 
-    ${CMAKE_CURRENT_LIST_DIR}/ddesktopentry.cpp 
-    ${CMAKE_CURRENT_LIST_DIR}/dtkcore_global.cpp
-  )
-  set(OUTER_HEADER 
-    ${CMAKE_CURRENT_LIST_DIR}/../include/global/dtkcore_global.h 
-    ${CMAKE_CURRENT_LIST_DIR}/../include/dconfig.h 
-    ${CMAKE_CURRENT_LIST_DIR}/../include/dsgapplication.h 
-    ${CMAKE_CURRENT_LIST_DIR}/../include/dsysinfo.h 
-    ${CMAKE_CURRENT_LIST_DIR}/../include/dsecurestring.h 
-    ${CMAKE_CURRENT_LIST_DIR}/../include/ddesktopentry.h
-  )
+    add_definitions(-DD_DISABLE_DCONFIG)
 endif()
-set(glob_SRC
+
+list(APPEND glob_SRC
   ${OUTER_HEADER}
   ${OUTER_SOURCE}
 )
