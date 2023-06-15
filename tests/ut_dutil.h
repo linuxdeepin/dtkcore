2df5ca70dd1e4fa87461b1d6f229e7abeae8614e
heyuming <heyuming@uniontech.com>
2023-06-15 13:59:23 +0800

    feat: add DThreadUtils class
    Log: 添加DThreadUtils类

--- tests/ut_dutil.h
+++ tests/ut_dutil.h
@@ -1,13 +1,14 @@
-// SPDX-FileCopyrightText: 2017 - 2022 UnionTech Software Technology Co., Ltd.
+// SPDX-FileCopyrightText: 2017 - 2023 UnionTech Software Technology Co., Ltd.
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
 #pragma once
-
+#include "dtkcore_global.h"
 #include <gtest/gtest.h>
+#if DTK_VERSION < DTK_VERSION_CHECK(6, 0, 0, 0)
 #include "dtimedloop.h"
-
 DCORE_USE_NAMESPACE
+#endif
 
 // 有返回值的 lambda 表达式、函数里面使用 ASSERT_XXX
 // 总之，不管有没有返回值，用它就对了
