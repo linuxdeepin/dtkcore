3a661dfff071eedf9841dd7ac9d99c4beead85a3
ck <chenke@deepin.org>
2023-07-10 08:49:58 +0800

    chore: add ut for DDbusSender
    - tweek FakeDBusService. add method and read/write propery
    - test DDbusSender. call dbus method and get/set dbus property

--- tests/fakedbus/fakedbusservice.h
+++ tests/fakedbus/fakedbusservice.h
@@ -6,6 +6,7 @@
 
 #include <QStringList>
 #include <QDBusObjectPath>
+
 #include <ddbusinterface.h>
 
 using Dtk::Core::DDBusInterface;
@@ -32,21 +33,29 @@ public:
     explicit FakeDBusService(QObject *parent = nullptr);
     ~FakeDBusService();
 
-    Q_PROPERTY(QString strProperty READ strproperty)
-    inline QString strproperty() { return m_strproperty; }
-
-    Q_PROPERTY(QList<QDBusObjectPath> objectPaths READ objectpaths);
-    inline QList<QDBusObjectPath> objectpaths() { return m_objectpaths; }
+    Q_PROPERTY(QString strProperty READ strproperty WRITE setStrproperty)
+    Q_PROPERTY(QList<QDBusObjectPath> objectPaths READ objectpaths)
 
     static QString get_service() { return "org.deepin.FakeDBusService"; }
     static QString get_path() { return "/org/deepin/FakeDBusService"; }
     static QString get_interface() { return "org.deepin.FakeDBusService"; }
 
+public Q_SLOTS:
+    inline QString strproperty() { return m_strproperty; }
+    void setStrproperty(const QString &var) {
+        if (var != m_strproperty)
+            m_strproperty = var;
+    }
+    inline QList<QDBusObjectPath> objectpaths() { return m_objectpaths; }
+    // ExportScriptableContents
+    Q_SCRIPTABLE QString foo() { return QString("bar"); }
+
 private:
     const QString m_service{"org.deepin.FakeDBusService"};
     const QString m_path{"/org/deepin/FakeDBusService"};
-    QString m_strproperty;
-    QList<QDBusObjectPath> m_objectpaths;
     void registerService();
     void unregisterService();
+
+    QString m_strproperty;
+    QList<QDBusObjectPath> m_objectpaths;
 };
