8ba05a2f808d789adfe8a5f2c7c5806655f84cc4
YinJie <yinjie@uniontech.com>
2023-12-20 15:17:10 +0800

    feat: 增加registerLoggingRulesWatcher接口
    功能:监听 dconfig 的值然后设置应用的日志级别
    目的:
    1.取消QT_LOGGING_RULES需要在创建 application之前处理
    因为QLoggingRegistry在构造函数中获取环境变量的值后不会监听环境变量的变化.
    目前直接使用applicationName当做appId 的做法不可行.
    2.不和应用的 applicationName耦合目前大部分应用的applicationName和dconfig
    的appId是不一样的,需要修改应用的名字
    可能存在多个 application 使用通过一个 dconfig
    的情况,修改applicantionName的方法不可行.
    PS:从 DConfig 获取默认 appId 的逻辑上看, DSGApplication应用会有问题
    3.开发者可以选择是否需要设置以及监听日志级别变动
    
    Log:
    Task: https://pms.uniontech.com/task-view-303379.html

--- include/log/LogManager.h
+++ include/log/LogManager.h
@@ -31,10 +31,18 @@ public:
 
     static void setLogFormat(const QString &format);
 
+    /*!
+     * \brief 监听 org.deepin.dtk.log 的变化动态调整应用的日志输出规则
+     * 此方法应该在创建 QApplication 之前调用，否则 QT_LOGGING_RULES 环境变量会覆盖 dconfig 的的值
+     * \a logFilePath 指定 dconfig 的 appId
+     */
+    static void registerLoggingRulesWatcher(const QString &appId);
+
 private:
     void initConsoleAppender();
     void initRollingFileAppender();
     void initJournalAppender();
+    void initLoggingRules(const QString &appId);
     QString joinPath(const QString &path, const QString &fileName);
 
     inline static DLogManager* instance(){
